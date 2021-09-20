6f218f8f1e5b2ac0f5b341315d6151d1d8e28b84
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2021-09-19 17:06:04 -0700

    Added a space when initilizing random var

--- CardPicker.cs
+++ CardPicker.cs
@@ -9,6 +9,7 @@ namespace PickRandomCards
     class CardPicker
     {
         static Random random = new Random();
+
         public static string[] PickSomeCards(int numberOfCards)
         {
             string[] pickedCards = new string[numberOfCards];
